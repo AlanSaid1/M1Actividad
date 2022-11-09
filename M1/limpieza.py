29a7f62cf251bfe6acc55ebfde88895ee88edb4d
A01752789 <69563637+A01752789@users.noreply.github.com>
2022-11-08 21:00:38 -0600

    correcciones mypy

--- M1/limpieza.py
+++ M1/limpieza.py
@@ -1,39 +1,41 @@
-#--------------------------------------------------------------
-# M1. Actividad 
+# --------------------------------------------------------------
+# M1. Actividad
 #
 # Fecha: 07-Nov-2022
 # Autores:
 #           A01746210 Alan Said Martinez Guzman
 #           A01752789 Luis Humberto Romero Perez
-# 
+#
 # Simulación de agentes capaces de limpiar celdas de una tupla
 # que contienen objetos situados aleatoriamente. Siendo los
 # movimientos de los agentes automatizados logrando moverse por
 # toda la tupla hasta lograr limpiarla por completo o que el tiempo
 # se haya agotado.
-#--------------------------------------------------------------
+# --------------------------------------------------------------
 
 from mesa import Agent, Model
 from mesa.space import MultiGrid
 from mesa.time import SimultaneousActivation
-import numpy as np
 
-#Estas librerias contienen 3 metodos; agents, scheduler y model
-#Agents: Permite manipular a los agentes en tanto a sus movimientos y caracteristicas.
-#Scheduler: Es un modelo especial que controla el orden en el que los agentes son activados.
-#Model: Es la visualización.
+# Estas librerias contienen 3 metodos; agents, scheduler y model
+# Agents: Permite manipular a los agentes en tanto a sus movimientos y 
+# caracteristicas.
+# Scheduler: Es un modelo especial que controla el orden en el que los 
+# agentes son activados.
+# Model: Es la visualización.
+
 
 class Celda(Agent):
-    #Constructor que permite que la clase inicialice los atributos del objeto.
+    # Constructor que permite que la clase inicialice los atributos del objeto.
     def __init__(self, unique_id, model):
-        #Representa la clase padre.
+        # Representa la clase padre.
         super().__init__(unique_id, model)
         self.estado = ''
         self.sigEstado = ''
 
-    #La función limpia la celda.
-    #Recibe como parametros la lista de los agentes contenidos en los nodos.
-    #Regresa una actualización del conteo de las celdas ya limpias.
+    # La función limpia la celda.
+    # Recibe como parametros la lista de los agentes contenidos en los nodos.
+    # Regresa una actualización del conteo de las celdas ya limpias.
     def step(self):
         contenido = len(self.model.grid.get_cell_list_contents(self.pos))
         if (contenido > 1):
@@ -47,46 +49,54 @@ class Celda(Agent):
     def advance(self):
         self.estado = self.sigEstado
 
+
 class limpiadorAgente(Agent):
-    #Funcion que inicializa las variables que contiene cada uno de los agentes.
-    #Lleva un registro del estado del agente respecto a las casillas.
-    #En el modelo regresa la cantidad de movimientos realizados por cada agente.
+    # Funcion que inicializa las variables que contiene cada uno
+    # de los agentes. Lleva un registro del estado del agente
+    # respecto a las casillas. En el modelo regresa la cantidad
+    # de movimientos realizados por cada agente.
     def __init__(self, unique_id: int, model: Model) -> None:
-        #Representa la clase padre.
+        # Representa la clase padre.
         super().__init__(unique_id, model)
         self.sigEstado = None
         self.estado = 'limpiador'
         self.movimientos = 0
 
-    #Limpia la celda sucia al agente caer en esta.
+    # Limpia la celda sucia al agente caer en esta.
     def step(self):
 
         if (self.pos in self.model.coordenadasSucias):
             self.model.coordenadasSucias.remove(self.pos)
-        #El agente se mueve.
+            self.sigEstado = self.pos
+        # El agente se mueve.
         else:
             vecinos = self.model.grid.get_neighborhood(self.pos, moore=True,
-                                                        include_center=False)
-            #Al incluir una propiedad de tipo random, el agente elige una de las 8 direcciones para moverse.
+                                                       include_center=False)
+            # Al incluir una propiedad de tipo random, el agente elige una de
+            # las 8 direcciones para moverse.
             nuevaPosicion = self.random.choice(vecinos)
             contenido = self.model.grid.get_cell_list_contents(nuevaPosicion)
             if (not self.model.grid.out_of_bounds(nuevaPosicion) and
                     ((len(contenido) <= 1))):
                 self.sigEstado = nuevaPosicion
+                self.model.grid.move_agent(self, self.sigEstado)
                 self.movimientos += 1
             else:
                 self.sigEstado = self.pos
-    #Avanza a la siguiente casilla de la tupla.
+
+    # Avanza a la siguiente casilla de la tupla.
     def advance(self):
         self.model.grid.move_agent(self, self.sigEstado)
 
+
 class limpiadorModelo(Model):
-    #Funcion que inicializa los variables en el modelo limpiador.
-    #Lleva un registro del tiempo, celdas sucias y limpias.
-    #Estas variables al finalizar el programa nos regresan un control de lo obtenido con el modelo.
+    # Funcion que inicializa los variables en el modelo limpiador.
+    # Lleva un registro del tiempo, celdas sucias y limpias.
+    # Estas variables al finalizar el programa nos regresan un control de
+    # lo obtenido con el modelo.
     def __init__(self, ancho, alto, numAgentes, suciedad, tiempoMax):
         self.numAgentes = numAgentes
-        self.grid = MultiGrid(ancho, alto, torus = False)
+        self.grid = MultiGrid(ancho, alto, torus=False)
         self.schedule = SimultaneousActivation(self)
         self.running = True
         self.celdasSucias = (ancho * alto * suciedad) // 100
@@ -95,7 +105,8 @@ class limpiadorModelo(Model):
         self.tiempoMax = tiempoMax
         self.tiempo = tiempoMax
 
-        # Crear y registrar las celdas sucias tomando en cuenta nuestro alto y ancho de tupla aleatoriamente.
+        # Crear y registrar las celdas sucias tomando en cuenta nuestro alto 
+        # y ancho de tupla aleatoriamente.
         id = 0
         for _ in range(self.celdasSucias):
             X = self.random.randrange(ancho)
@@ -110,7 +121,7 @@ class limpiadorModelo(Model):
             self.grid.place_agent(a, (X, Y))
             id += 1
 
-        #Inicializar agentes.
+        # Inicializar agentes.
         for _ in range(numAgentes):
             a = limpiadorAgente(id, self)
             self.schedule.add(a)
@@ -118,7 +129,8 @@ class limpiadorModelo(Model):
             id += 1
 
     def step(self):
-        #Al ya no existir coordenadas sucias o se haya llegado al tiempo máximo posible, termina el programa.
+        # Al ya no existir coordenadas sucias o se haya llegado al tiempo
+        # máximo posible, termina el programa.
         if (len(self.coordenadasSucias) <= 0 or self.tiempo <= 0):
             self.running = False
             print(f"Tiempo que tardo en limpiar: \
@@ -128,4 +140,4 @@ class limpiadorModelo(Model):
             return
         else:
             self.tiempo -= 1
-            self.schedule.step()
+        self.schedule.step()
--- M1/limpiezaViz.py
+++ M1/limpiezaViz.py
@@ -1,37 +1,38 @@
-#--------------------------------------------------------------
-# M1. Actividad 
+# --------------------------------------------------------------
+# M1. Actividad
 #
 # Fecha: 07-Nov-2022
 # Autores:
 #           A01746210 Alan Said Martinez Guzman
 #           A01752789 Luis Humberto Romero Perez
-# 
+#
 # Simulación de agentes capaces de limpiar celdas de una tupla
 # que contienen objetos situados aleatoriamente. Siendo los
 # movimientos de los agentes automatizados logrando moverse por
 # toda la tupla hasta lograr limpiarla por completo o que el tiempo
 # se haya agotado.
-#--------------------------------------------------------------
+# --------------------------------------------------------------
 
 from limpieza import *
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 
-#Propiedades de los agentes:
+# Propiedades de los agentes:
 
-#Diccionario capaz de convertirse en objeto JSON.
-#El parametro "agent" como el actor dentro de la tupla.
+
+# Diccionario capaz de convertirse en objeto JSON.
+# El parametro "agent" como el actor dentro de la tupla.
 # Retorna los agentes en las celdas.
 def agentPortrayal(agent):
     portrayal = {
                 "Shape": "circle",
-                 "Filled": "true",
-                 "Layer": 0,
-                 "Color": "white",
-                 "r": 0.7
+                "Filled": "true",
+                "Layer": 0,
+                "Color": "white",
+                "r": 0.7
                 }
 
-#Representa a los actores con color verde.
+# Representa a los actores con color verde.
     if agent.estado == 'limpiador':
         portrayal["Color"] = "green"
         portrayal["Layer"] = 2
@@ -39,13 +40,13 @@ def agentPortrayal(agent):
         portrayal["text"] = agent.movimientos
         portrayal["text_color"] = "black"
 
-#Representa a las celdas sucias con color cafe.
+# Representa a las celdas sucias con color cafe.
     elif agent.estado == 'sucia':
         portrayal["Color"] = "brown"
         portrayal["Layer"] = 0
         portrayal["r"] = 0.7
 
-#Al limpiar las celdas estas se convierten en color blanco.
+# Al limpiar las celdas estas se convierten en color blanco.
     elif agent.estado == 'limpia':
         portrayal["Color"] = "white"
         portrayal["Layer"] = 1
@@ -53,11 +54,13 @@ def agentPortrayal(agent):
 
     return portrayal
 
-#La variable grid dibuja las medidas de la tupla, asi como de los pixeles.
+
+# La variable grid dibuja las medidas de la tupla, asi como de los pixeles.
 ancho = 10
 alto = 10
 grid = CanvasGrid(agentPortrayal, ancho, alto, 750, 750)
-#Una vez que el servidor es creado, establecemos el puerto en el que escuchará.
+# Una vez que el servidor es creado, establecemos el
+# puerto en el que escuchará.
 server = ModularServer(limpiadorModelo,
                        [grid],
                        "Dirty Cleaner Mode",
